f3a2a4ee94ed91b83c778948e2968901b7954c09
m0use-dev <114124358+m0use-dev@users.noreply.github.com>
2023-11-19 15:46:32 +0900

    Springのバナーを非表示

--- src/main/java/com/example/todoapp/ToDoAppApplication.java
+++ src/main/java/com/example/todoapp/ToDoAppApplication.java
@@ -2,12 +2,15 @@ package com.example.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.Banner.Mode;
 
 @SpringBootApplication
 public class ToDoAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ToDoAppApplication.class, args);
+		SpringApplication app = new SpringApplication(ToDoAppApplication.class);
+		app.setBannerMode(Mode.OFF);
+		app.run(args);
 	}
 
 }
