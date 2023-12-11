48823b3c6aa4ad52897023b27fee3b473658da9e
m0use-dev <114124358+m0use-dev@users.noreply.github.com>
2023-12-11 21:00:29 +0900

    アカウントパスワードのエンコード

--- src/main/java/com/example/todoapp/SampleSecurityConfig.java
+++ src/main/java/com/example/todoapp/SampleSecurityConfig.java
@@ -5,10 +5,12 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,15 +25,15 @@ public class SampleSecurityConfig {
     @Autowired
     private DataSource dataSource;
 
-//    検証用　エンコードをオフ
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http)
             throws Exception {
+
         http.csrf(
                 csrf -> csrf
                         .ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")));
