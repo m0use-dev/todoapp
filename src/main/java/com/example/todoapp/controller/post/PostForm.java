81ff84e25a29051de0bafe5ca846797c65456693
m0use-dev <114124358+m0use-dev@users.noreply.github.com>
2023-12-15 21:27:50 +0900

    フォームを整理

--- src/main/java/com/example/todoapp/controller/post/PostForm.java
+++ src/main/java/com/example/todoapp/controller/post/PostForm.java
@@ -1,11 +1,15 @@
 package com.example.todoapp.controller.post;
 
 import com.example.todoapp.service.post.PostEntity;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 public record PostForm(
+        @NotBlank
+        @Size(max = 256,message="256文字以内で入力してください")
         String content,
         LocalDate deadline
 ){
