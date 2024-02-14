349bdab58859050ec508ca4e047b36bf66b9c6f5
m0use-dev <114124358+m0use-dev@users.noreply.github.com>
2024-02-14 23:00:33 +0900

    不要コードの削除

--- src/main/java/com/example/todoapp/repository/user/UserRepository.java
+++ src/main/java/com/example/todoapp/repository/user/UserRepository.java
@@ -7,16 +7,9 @@ import java.util.List;
 
 @Mapper
 public interface UserRepository {
-    @Select("SELECT id,username,authority FROM authorities WHERE user_id = 1;")
-    List<UserEntity> getUser();
+
 
     @Select("SELECT id,username,authority FROM authorities;")
     List<UserEntity> getUserAll();
-
-    @Insert("INSERT INTO user(name, password, authority) VALUES(#{name}, ?, ?)")
-    int insertUser(String name, String password, String authority);
-
-    @Select("SELECT id FROM authorities WHERE username = #{username};")
-    int getUserId(String username);
-
+    
 }
