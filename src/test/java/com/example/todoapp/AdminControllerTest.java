a2dc16cebee49e34aee0a36e478c7bc8c3112e9c
m0use-dev <114124358+m0use-dev@users.noreply.github.com>
2024-02-28 22:47:55 +0900

    管理者ページのテスト

---
+++ src/test/java/com/example/todoapp/AdminControllerTest.java
@@ -0,0 +1,68 @@
+package com.example.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AdminControllerTest {
+    @WithMockUser(roles="ADMIN")
+    @Test
+    void 管理者権限が管理者用ページにアクセス可能確認(@Autowired MockMvc mvc) throws Exception {
+        mvc.perform(
+                        MockMvcRequestBuilders.get("/admin"))
+                .andExpect(MockMvcResultMatchers.status().isOk()
+                );
+    }
+
+    @WithMockUser(roles="USER")
+    @Test
+    void ユーザー権限が管理者用ページにアクセス不可確認(@Autowired MockMvc mvc) throws Exception {
+        mvc.perform(
+                        MockMvcRequestBuilders.get("/admin"))
+                .andExpect(MockMvcResultMatchers.status().isForbidden()
+                );
+    }
+
+    @WithMockUser(roles="ADMIN")
+    @Test
+    void 管理者権限が管理者用投稿一覧ページにアクセス可能確認(@Autowired MockMvc mvc) throws Exception {
+        mvc.perform(
+                        MockMvcRequestBuilders.get("/admin/posts"))
+                .andExpect(MockMvcResultMatchers.status().isOk()
+                );
+    }
+
+    @WithMockUser(roles="USER")
+    @Test
+    void ユーザー権限が管理者用投稿一覧ページにアクセス不可確認(@Autowired MockMvc mvc) throws Exception {
+        mvc.perform(
+                        MockMvcRequestBuilders.get("/admin/posts"))
+                .andExpect(MockMvcResultMatchers.status().isForbidden()
+                );
+    }
+
+    @WithMockUser(roles="ADMIN")
+    @Test
+    void 管理者権限が管理者用ユーザー一覧ページにアクセス可能確認(@Autowired MockMvc mvc) throws Exception {
+        mvc.perform(
+                        MockMvcRequestBuilders.get("/admin/users"))
+                .andExpect(MockMvcResultMatchers.status().isOk()
+                );
+    }
+
+    @WithMockUser(roles="USER")
+    @Test
+    void ユーザー権限が管理者用ユーザー一覧ページにアクセス不可確認(@Autowired MockMvc mvc) throws Exception {
+        mvc.perform(
+                        MockMvcRequestBuilders.get("/admin/users"))
+                .andExpect(MockMvcResultMatchers.status().isForbidden()
+                );
+    }
+}
