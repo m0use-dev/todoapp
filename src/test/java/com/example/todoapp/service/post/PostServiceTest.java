66f3fca07b643dc8f1ea11009e0823a24a392fdd
m0use-dev <114124358+m0use-dev@users.noreply.github.com>
2024-03-02 16:40:26 +0900

    ＠Autowiredに変更

--- src/test/java/com/example/todoapp/service/post/PostServiceTest.java
+++ src/test/java/com/example/todoapp/service/post/PostServiceTest.java
@@ -1,31 +1,29 @@
 package com.example.todoapp.service.post;
 
+import com.example.todoapp.repository.post.PostRepository;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
 public class PostServiceTest {
-    
+    @Autowired
+    private PostService postService = new PostService(null);
+
     @Test
     @DisplayName("getLastWeekメソッドの動作確認")
     void getLastWeekメソッドの動作確認() {
-        PostService postService = new PostService(null);
         LocalDate today = LocalDate.of(2024, 3, 2);
         LocalDate expected = LocalDate.of(2024, 2, 24);
         LocalDate actual = postService.getLastWeek(today);
         assertEquals(expected, actual);
     }
-    @Test
-    @DisplayName("データベースチェック")
-    @DatabaseSetup("sampleData.xml")
-    void データベースチェック() {
-        PostService postService = new PostService(null);
-        Optional<PostEntity> expected = postService.getPost(1);
-//        Optional<PostEntity> actual = postService.getPost(1);
-//        assertEquals(expected, actual);
-    }
+
 }
