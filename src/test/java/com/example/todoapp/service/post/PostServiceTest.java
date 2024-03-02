17e61996d8b84247a82ee88e1556ae79d052bfdc
m0use-dev <114124358+m0use-dev@users.noreply.github.com>
2024-03-02 16:11:57 +0900

    リネーム

--- src/test/java/com/example/todoapp/service/post/PostServiceTest.java
+++ src/test/java/com/example/todoapp/service/post/PostServiceTest.java
@@ -1,12 +1,15 @@
 package com.example.todoapp.service.post;
 
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 public class PostServiceTest {
+    
     @Test
     @DisplayName("getLastWeekメソッドの動作確認")
     void getLastWeekメソッドの動作確認() {
@@ -15,5 +18,14 @@ public class PostServiceTest {
         LocalDate expected = LocalDate.of(2024, 2, 24);
         LocalDate actual = postService.getLastWeek(today);
         assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("データベースチェック")
+    @DatabaseSetup("sampleData.xml")
+    void データベースチェック() {
+        PostService postService = new PostService(null);
+        Optional<PostEntity> expected = postService.getPost(1);
+//        Optional<PostEntity> actual = postService.getPost(1);
+//        assertEquals(expected, actual);
     }
 }
---
+++ src/test/java/resources/schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT,
+    username VARCHAR(255) PRIMARY KEY NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    enabled TINYINT NOT NULL
+);
+
+CREATE TABLE authorities (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    authority enum('ROLE_ADMIN', 'ROLE_USER') NOT NULL
+);
+
+CREATE TABLE posts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    content VARCHAR(255) NOT NULL,
+    status VARCHAR(10) NOT NULL,
+    created_at DATE NOT NULL,
+    updated_at DATE NOT NULL,
+    deadline DATE,
+    FOREIGN KEY (username) REFERENCES users(username)
+);
