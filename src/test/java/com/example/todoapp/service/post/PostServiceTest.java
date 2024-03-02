cc127271898b8be490b8f1ac83c7ba3af7cc3b53
m0use-dev <114124358+m0use-dev@users.noreply.github.com>
2024-03-02 10:59:25 +0900

    テストコード「getLastWeekメソッドの動作確認」を追加

--- src/test/java/com/example/todoapp/service/post/PostServiceTest.java
+++ src/test/java/com/example/todoapp/service/post/PostServiceTest.java
@@ -1,4 +1,19 @@
 package com.example.todoapp.service.post;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
 public class PostServiceTest {
+    @Test
+    @DisplayName("getLastWeekメソッドの動作確認")
+    void getLastWeek(){
+        PostService postService = new PostService(null);
+        LocalDate today = LocalDate.of(2024, 3, 2);
+        LocalDate expected = LocalDate.of(2024, 2, 24);
+        LocalDate actual = postService.getLastWeek(today);
+        assertEquals(expected, actual);
+    }
 }
