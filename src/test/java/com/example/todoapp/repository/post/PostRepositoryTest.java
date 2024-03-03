857fad8ed3a7420aac39001ab97ead4e946d49b8
m0use-dev <114124358+m0use-dev@users.noreply.github.com>
2024-03-03 20:50:56 +0900

    getContributorメソッドの動作確認

--- src/test/java/com/example/todoapp/repository/post/PostRepositoryTest.java
+++ src/test/java/com/example/todoapp/repository/post/PostRepositoryTest.java
@@ -1,7 +1,26 @@
 package com.example.todoapp.repository.post;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
 class PostRepositoryTest {
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostRepositoryTest(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Test
+    @DatabaseSetup("sampleData.xml")
+    public void getContributorメソッドの動作確認() {
+        String result = postRepository.getContributor(1);
+        assertThat(result).isEqualTo("user1");
+    }
 
 }
--- src/test/java/com/example/todoapp/repository/user/UserRepositoryTest.java
+++ src/test/java/com/example/todoapp/repository/user/UserRepositoryTest.java
@@ -1,7 +1,27 @@
 package com.example.todoapp.repository.user;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.example.todoapp.service.user.UserEntity;
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
 class UserRepositoryTest {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserRepositoryTest(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
 }
