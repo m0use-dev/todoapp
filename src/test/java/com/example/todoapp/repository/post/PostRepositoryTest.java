1d768faec512d91b9a3374ad495cd0975e129468
m0use-dev <114124358+m0use-dev@users.noreply.github.com>
2024-03-05 22:41:01 +0900

    テスト表示名変更

--- src/test/java/com/example/todoapp/repository/post/PostRepositoryTest.java
+++ src/test/java/com/example/todoapp/repository/post/PostRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.example.todoapp.repository.post;
 
 import com.github.springtestdbunit.annotation.DatabaseSetup;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,9 +19,51 @@ class PostRepositoryTest {
 
     @Test
     @DatabaseSetup("sampleData.xml")
+    @DisplayName("")
+    public void getPostsメソッドの動作確認() {
+    }
+
+    @Test
+    @DatabaseSetup("sampleData.xml")
+    @DisplayName("")
+    public void getPostsAllメソッドの動作確認() {
+    }
+
+    @Test
+    @DatabaseSetup("sampleData.xml")
+    @DisplayName("")
+    public void getPostメソッドの動作確認() {
+    }
+
+    @Test
+    @DatabaseSetup("sampleData.xml")
+    @DisplayName("")
     public void getContributorメソッドの動作確認() {
         String result = postRepository.getContributor(1);
         assertThat(result).isEqualTo("user1");
     }
 
+    @Test
+    @DatabaseSetup("sampleData.xml")
+    @DisplayName("")
+    public void insertPostメソッドの動作確認() {
+    }
+
+    @Test
+    @DatabaseSetup("sampleData.xml")
+    @DisplayName("")
+    public void deletePostメソッドの動作確認() {
+    }
+
+    @Test
+    @DatabaseSetup("sampleData.xml")
+    @DisplayName("completionPostメソッドの動作確認")
+    public void completionPostメソッドの動作確認() {
+    }
+
+    @Test
+    @DatabaseSetup("sampleData.xml")
+    @DisplayName("updatePostメソッドの動作確認")
+    public void updatePostメソッドの動作確認() {
+    }
 }
