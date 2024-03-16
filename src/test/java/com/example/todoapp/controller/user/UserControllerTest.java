7c98640b7b91554890ed1e61b8086266bad9476c
m0use-dev <114124358+m0use-dev@users.noreply.github.com>
2024-03-16 23:27:06 +0900

    テスト関連を修正

--- src/test/java/com/example/todoapp/controller/user/UserControllerTest.java
+++ src/test/java/com/example/todoapp/controller/user/UserControllerTest.java
@@ -6,13 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @SpringBootTest
-@Transactional
 @AutoConfigureMockMvc
 public class UserControllerTest {
     @Test
